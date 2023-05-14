1f238c6f4cee006adff28f1caf156da83b8f1863
Sasa Savic Perusina <sasa@bubulazy.com>
2023-05-14 18:09:30 +0100

    Finished, almost

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,28 @@
+import { useState } from "react";
+import { ReactMarkdown } from "react-markdown/lib/react-markdown";
+import data from "./assets/data";
+
 function App() {
-  return <h1>jfghsdg sdfhg sdfg sdf sjkdhfg hsjd</h1>;
+  const [form, setForm] = useState(data);
+
+  function handleChange(e) {
+    e.preventDefault();
+    setForm(e.target.value);
+  }
+
+  return (
+    <div className="container">
+      <textarea
+        onChange={handleChange}
+        value={form}
+        id="editor"
+        name="input-md"
+      />
+      <div id="preview">
+        <ReactMarkdown>{form}</ReactMarkdown>
+      </div>
+    </div>
+  );
 }
 
 export default App;
